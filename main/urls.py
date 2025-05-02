c4b6cde1619a9c97b2f577df5df11ffc487c1c30
pratikshasarukte <157965887+pratikshasarukte@users.noreply.github.com>
2025-05-02 21:35:14 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.cart, name='cart'),
     path('display/', views.displayTemplates, name='display'),
+     path('about/', views.about, name='about'),
+     path('collections/', views.collections, name='collections'),
 
     path('api/add-remove-cart', views.addRemoveCart),
-    path('api/update-product-quantity', views.updateProductQuantity)
+    path('api/update-product-quantity', views.updateProductQuantity),
+   
 ]
 
